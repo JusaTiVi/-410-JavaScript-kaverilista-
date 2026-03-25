a6d818c57b0508b47d12b86c79cd3afdee83e8ce
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-03-25 13:12:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function annaNimi() {
+const nimet = []
+let length = nimet.length;
+if (length = 10) {
+    return
+}
+
+else {
+        let person = prompt("anna nimi")
+        nimet.push(person)
+}
+
+}
